4b66470b16aa6620614f64849743abb7f7c83cc5
SteshM <mbithestella0@gmail.com>
2024-03-07 00:10:48 +0300

    created studentEntity and repo

---
+++ src/main/java/com/example/FinalYearProj/entities/StudentEntity.java
@@ -0,0 +1,18 @@
+package com.example.FinalYearProj.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "student")
+public class StudentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long studentId;
+    private long admissionNo;
+}
---
+++ src/main/java/com/example/FinalYearProj/repos/StudentRepo.java
@@ -0,0 +1,9 @@
+package com.example.FinalYearProj.repos;
+
+import com.example.FinalYearProj.entities.StudentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepo extends JpaRepository<StudentEntity , Long> {
+}
