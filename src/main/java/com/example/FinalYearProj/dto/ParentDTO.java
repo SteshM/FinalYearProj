0615ce94f33c984703f6b47ab4555ee2f41eab57
SteshM <mbithestella0@gmail.com>
2024-03-06 23:28:31 +0300

    created tutor and parent DTOs

---
+++ src/main/java/com/example/FinalYearProj/dto/ParentDTO.java
@@ -0,0 +1,11 @@
+package com.example.FinalYearProj.dto;
+
+import lombok.Data;
+
+@Data
+public class ParentDTO {
+    private long idNo;
+    private long contact;
+
+
+}
---
+++ src/main/java/com/example/FinalYearProj/dto/TutorDTO.java
@@ -0,0 +1,10 @@
+package com.example.FinalYearProj.dto;
+
+import lombok.Data;
+
+@Data
+public class TutorDTO {
+    private String employmentNo;
+    private long contact;
+    private long idNo;
+}
